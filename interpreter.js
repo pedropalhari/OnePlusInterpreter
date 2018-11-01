034c5e4e8d6e60021ed12bc62b949a7319e943a6
Pedro Palhari <pedropalhari@gmail.com>
2018-11-01 12:19:43 -0200

    Interpretador terminado

--- interpreter.js
+++ interpreter.js
@@ -1,7 +1,7 @@
 let programStack = [];
 let functionMap = {};
 
-let program = "111++;";
+let program = "11-1;#";
 
 let PC = 0;
 
@@ -20,8 +20,54 @@ while (PC < program.length) {
       programStack.push(x + y);
       break;
 
-    case ':':
-    case ';':
+    case "-":
+      x = programStack.pop();
+      y = programStack.pop();
+      programStack.push(x - y);
+      break;
+
+    case "*":
+      x = programStack.pop();
+      y = programStack.pop();
+      programStack.push(parseInt(x * y));
+      break;
+
+    case "|":
+      x = programStack.pop();
+      y = programStack.pop();
+      programStack.push(parseInt(x / y));
+      break;
+
+    case "/":
+      x = programStack.pop();
+      programStack.unshift(x);
+      break;
+
+    case "\\":
+      x = programStack.shift();
+      programStack.push(x);
+      break;
+
+    case "^":
+      x = programStack.pop();
+      y = programStack.pop();
+      programStack.push(x);
+      programStack.push(y);
+      break;
+
+    case "<":
+      x = programStack.pop();
+      y = programStack.pop();
+      programStack.push(x >= y ? 1 : 0);
+      break;
+
+    case "#":
+      x = programStack.pop();
+      PC = x;
+      break;
+
+    case ":":
+    case ";":
       x = programStack.pop();
       console.log(x);
       break;
