b43267e4bba50d9f41c36cd2d2476c57e26c36e2
Pedro Palhari <pedropalhari@gmail.com>
2018-11-05 11:39:36 -0200

    Mostrando loop de 5 steps

--- interpreter.js
+++ interpreter.js
@@ -1,78 +1,140 @@
+const fs = require("fs");
+var readline = require("readline");
+
+const DEBUG = false;
+
+async function readStep() {
+  return new Promise(resolve => {
+    var rl = readline.createInterface({
+      input: process.stdin,
+      output: process.stdout,
+      terminal: false
+    });
+
+    rl.on("line", function(cmd) {
+      resolve();
+      rl.close();
+    });
+  });
+}
+
+let program = fs.readFileSync(`${process.argv[2]}`).toString();
+
 let programStack = [];
 let functionMap = {};
 
-let program = "11-1;#";
-
 let PC = 0;
 
-while (PC < program.length) {
-  let command = program[PC++];
-  let x, y;
+function removeComments(text = "") {
+  let start, end;
 
-  switch (command) {
-    case "1":
-      programStack.push(1);
-      break;
+  while (text.indexOf(" ") != -1) text = text.replace(" ", "");
 
-    case "+":
-      x = programStack.pop();
-      y = programStack.pop();
-      programStack.push(x + y);
-      break;
+  while (text.indexOf("\n") != -1) text = text.replace("\n", "");
 
-    case "-":
-      x = programStack.pop();
-      y = programStack.pop();
-      programStack.push(x - y);
-      break;
+  do {
+    start = text.indexOf("[");
+    end = text.indexOf("]");
+    text = text.substring(0, start) + text.substring(end + 1);
+  } while (start != -1);
 
-    case "*":
-      x = programStack.pop();
-      y = programStack.pop();
-      programStack.push(parseInt(x * y));
-      break;
-
-    case "|":
-      x = programStack.pop();
-      y = programStack.pop();
-      programStack.push(parseInt(x / y));
-      break;
-
-    case "/":
-      x = programStack.pop();
-      programStack.unshift(x);
-      break;
-
-    case "\\":
-      x = programStack.shift();
-      programStack.push(x);
-      break;
-
-    case "^":
-      x = programStack.pop();
-      y = programStack.pop();
-      programStack.push(x);
-      programStack.push(y);
-      break;
-
-    case "<":
-      x = programStack.pop();
-      y = programStack.pop();
-      programStack.push(x >= y ? 1 : 0);
-      break;
-
-    case "#":
-      x = programStack.pop();
-      PC = x;
-      break;
-
-    case ":":
-    case ";":
-      x = programStack.pop();
-      console.log(x);
-      break;
-
-    default:
-      break;
-  }
+  return text;
 }
+
+program = removeComments(program);
+
+let comment = false;
+(async () => {
+  while (PC < program.length) {
+    let command = program[PC++];
+    let x, y;
+
+    if (DEBUG) {
+      await readStep();
+      console.log(`PC: ${PC}`);
+      console.log(`STACK: ${programStack}`);
+      console.log(`COMMAND: ${command}`);
+    }
+
+    if (comment) {
+      if (command == "]") comment = false;
+      continue;
+    } else if (command == "[") {
+      comment = true;
+      continue;
+    }
+
+    switch (command) {
+      case "1":
+        programStack.push(1);
+        break;
+
+      case "+":
+        x = programStack.pop();
+        y = programStack.pop();
+        programStack.push(x + y);
+        break;
+
+      case "-":
+        x = programStack.pop();
+        y = programStack.pop();
+        programStack.push(x - y);
+        break;
+
+      case "*":
+        x = programStack.pop();
+        y = programStack.pop();
+        programStack.push(parseInt(x * y));
+        break;
+
+      case "|":
+        x = programStack.pop();
+        y = programStack.pop();
+        programStack.push(parseInt(x / y));
+        break;
+
+      case "/":
+        x = programStack.pop();
+        programStack.unshift(x);
+        break;
+
+      case "\\":
+        x = programStack.shift();
+        programStack.push(x);
+        break;
+
+      case "^":
+        x = programStack.pop();
+        y = programStack.pop();
+        programStack.push(x);
+        programStack.push(y);
+        break;
+
+      case "<":
+        x = programStack.pop();
+        y = programStack.pop();
+        programStack.push(x >= y ? 1 : 0);
+        break;
+
+      case "#":
+        x = programStack.pop();
+        PC += x;
+        break;
+
+      case '"':
+        x = programStack.pop();
+        programStack.push(x);
+        programStack.push(x);
+        break;
+
+      case ":":
+      case ";":
+        x = programStack.pop();
+        console.log(x);
+        break;
+
+      default:
+        break;
+    }
+  }
+})();
